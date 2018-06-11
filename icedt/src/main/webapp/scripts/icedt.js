a49a6444baa4396c4559121a8ccffe06676fe53f
Wang <Li.Wang2@unisys.com>
2018-06-11 16:36:57 -0400

    update datatable

--- icedt/src/main/webapp/scripts/icedt.js
+++ icedt/src/main/webapp/scripts/icedt.js
@@ -10,27 +10,34 @@ $(document).ready(function() {
             $('#example').DataTable( {
                 data: data,
 			    columns: [
+						{
+		            className:      'details-control',
+		            orderable:      false,
+		            data:           null,
+		            defaultContent: ''
+		        },
 			        { data: "id" },
 			        { data: "firstName" },
 			        { data: "lastName" },
 			        { data: "defactoId" },
 			        { data: "dob" },
 			        { data: "riskScore" }
-			    ]
+			    ],
+					"order": [[1, 'asc']]
 	        });
-		        
+
 	    },
 	    error: function (request, status, error) {
 	        alert(request.responseText);
 	    }
 	});
 
+  var detailRows = [];
 
-    
 	$('#example tbody').on('click', 'td.details-control', function () {
 	    var tr = $(this).closest('tr');
-	    var row = table.row( tr );
-	 
+	    var row = $('#example').DataTable().row( tr );
+
 	    if ( row.child.isShown() ) {
 	        row.child.hide();
 	        tr.removeClass('shown');
@@ -45,21 +52,20 @@ $(document).ready(function() {
 	function format ( rowData ) {
 	    var div = $('<div/>')
 	        .addClass( 'loading' )
-	        .text( 'Loading...' );
-	 
-	    $.ajax( {
-	        url: 'http://192.60.241.81:8080/Persons',
-	        data: {
-	            name: rowData.name
-	        },
-	        dataType: 'json',
-	        success: function ( json ) {
-	            div
-	                .html( json.html )
-	                .removeClass( 'loading' );
-	        }
-	    } );
-	 
+	        .text( 'Test' );
+
+					$.ajax({
+					    type: "get", url: "http://192.60.241.81:8080/Person/1915163",
+					    dataType: 'json',
+					    success: function (data, text) {
+					    	console.log("AppData from Server: " + JSON.stringify(data));
+                div.addClass( 'loading' ).text('id=' + data.id + 'firstName=' + data.firstName);
+					    },
+					    error: function (request, status, error) {
+					        alert('data issue');
+					    }
+					});
+
 	    return div;
 	}
 
